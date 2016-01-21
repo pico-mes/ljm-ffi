a6bc6c86805f1510b0bd8c1f427542954e8a5b43
chrisjohn404 <chris.s.johnson4@gmail.com>
2016-01-20 17:40:19 -0700

    Performed more openAll calls and added some text explaining what I was not expecting to see and wanting explanations about.

--- playground/open_device_and_perform_openall.js
+++ playground/open_device_and_perform_openall.js
@@ -13,17 +13,26 @@ var data;
 // Define a variable that will store the device's handle.
 var handle;
 
-// Open the first found T7.
-data = ljm.LJM_OpenS('LJM_dtT7', 'LJM_ctEthernet', 'LJM_idANY', 0);
+console.log('Please make sure that there are 2-3 T7\'s connected to your network');
+console.log();
 
-// Exit the program if a device was not found.
-if(data.ljmError !== 0) {
-	console.log('Failed to open a device, please connect a T7 to your computer.');
-	process.exit();
-} else {
-	handle = data.handle;
-	console.log('Connected to device:', getHandleInfoSync(handle));
+function openOneEthernetDevice() {
+	// Open the first found T7.
+	console.log('Opening the first found Ethernet device');
+	data = ljm.LJM_OpenS('LJM_dtT7', 'LJM_ctEthernet', 'LJM_idANY', 0);
+
+	// Exit the program if a device was not found.
+	if(data.ljmError !== 0) {
+		console.log('Failed to open a device, please connect a T7 to your computer.');
+		process.exit();
+	} else {
+		handle = data.handle;
+		console.log('Connected to device via OpenS:');
+		console.log(getHandleInfoSync(handle));
+		console.log();
+	}
 }
+openOneEthernetDevice();
 
 // Initialize an array of length 128.
 var aHandles = [];
@@ -44,6 +53,7 @@ function parseOpenAllData(openAllData, userStr) {
 }
 function performListAllTCP() {
 	// Perform the open-all call.
+	console.log('Performing OpenAll TCP')
 	openAllData = ljm.LJM_OpenAll(7, 2, 0, aHandles, 0,0, '');
 
 	// console.log('openAllData', openAllData);
@@ -52,14 +62,53 @@ function performListAllTCP() {
 
 function performListAllUDP() {
 	// Perform the open-all call.
+	console.log('Performing OpenAll UDP')
 	openAllData = ljm.LJM_OpenAll(7, 5, 0, aHandles, 0,0, '');
 
 	// console.log('openAllData', openAllData);
 	parseOpenAllData(openAllData, 'Opened Devices UDP:');
 }
 
+
 performListAllUDP();
+console.log('I Would have expected all of the devices to be found and UDP based Ethernet handles returned for the devices.');
+console.log('Unfortunately, only two new handles are created for an ethernet connection type.');
+console.log();
+
 performListAllTCP();
+console.log('No new handles are created after performing an OpenAll TCP call, the handle numbers are 1,2,3');
+console.log('I believe this means that the handles were upgraded to TCP handles?');
+console.log();
+
+performListAllUDP();
+console.log('Performed a second OpenAll UDP call.  The second call results in consistent results which is good.')
+console.log();
+
+performListAllTCP();
+console.log('Performed a second OpenAll TCP call.  The second call results in consistent results which is good.')
+console.log();
+
+console.log('Closing All handles via LJM_CloseAll...');
+data = ljm.LJM_CloseAll();
+console.log();
+
+openOneEthernetDevice();
+performListAllTCP();
+console.log('Why do these devices report a CT of 2 indicating a TCP connection type?');
+console.log('Shouldn\'t they report either 3 or 4?');
+console.log();
+
+performListAllUDP();
+console.log('Why aren\'t any handles returned?  The devices should still be discoverable via UDP.');
+console.log();
+
+performListAllTCP();
+console.log('Performed a second OpenAll TCP call.  The second call results in consistent results which is good.')
+console.log();
+
+performListAllUDP();
+console.log('Performed a second OpenAll UDP call.  The second call results in consistent results which is good.')
+console.log();
 
 
 
